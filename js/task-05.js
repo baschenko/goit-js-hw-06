33b9593c1d4f9bf629b4d16fb91c2de8bd52b5ee
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2023-03-22 00:29:08 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,7 +3,7 @@ const outputEl = document.querySelector('#name-output');
 
 const inputNameChange = (event) => {
     outputEl.textContent = event.currentTarget.value;
-   if (event.currentTarget.value === '') {
+   if (event.currentTarget.value.trim() === '') {
         outputEl.textContent = 'Anonymous';    
     } 
 }
