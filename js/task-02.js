8173e71236c7592bd37d523591cbeea4e003a971
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2023-03-22 00:29:05 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -9,13 +9,12 @@ const ingredients = [
 
 const ulIdEl = document.querySelector('#ingredients');
 
-const arrListItem=[];
-  
-ingredients.forEach(ingredient => {
+const arrListItem = ingredients.map(ingredient => {
   const listEl = document.createElement('li');
   listEl.classList.add('item');
   listEl.textContent = ingredient;
-  arrListItem.push(listEl);
+  return listEl
 })
+
 
 ulIdEl.append(...arrListItem);
